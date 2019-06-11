bd75b04b77cca56285f16da52ab0f76f8afd16ba
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2019-06-11 17:10:11 +0800

    Update webpack flow

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env, argv) => {
   const dev = argv.mode !== 'production'
@@ -9,9 +10,12 @@ module.exports = (env, argv) => {
     target: 'web',
     devtool: dev ? 'inline-source-map' : false,
     devServer: { contentBase: './' },
-    entry: { touring: './index.tsx' },
+    entry: {
+      touring: './src/index.tsx',
+      index: './docs/index.js'
+    },
     output: {
-      path: path.resolve(__dirname, './dist'),
+      path: path.resolve(__dirname, './'),
       library: 'touring',
       libraryExport: 'default',
       libraryTarget: 'umd',
@@ -21,11 +25,22 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         { test: /\.tsx?$/, use: { loader: 'ts-loader' } },
-        { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] }
+        { test: /\.scss$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'] },
+        { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+        { test: /\.svg/, use: { loader: 'svg-url-loader', options: { encoding: 'base64', stripdeclarations: true } }}
       ]
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' })
+      new MiniCssExtractPlugin({ filename: '[name].css', chunkFilename: '[id].css' }),
+      new HtmlWebpackPlugin({
+        title: 'touring | Interactive Product Tours',
+        template: 'docs/index.ejs',
+        filename: 'index.html',
+        meta: {
+          viewport: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0',
+          description: 'touring is a javascript/preact library that allows you to generate both beautiful and powerful product tours with ease.'
+        }
+      })
     ],
     optimization: {
       minimizer: [
